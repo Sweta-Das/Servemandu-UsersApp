e052e8c13e217f44cf1f8f84540b1a4b1a3c80aa
Sweta-Das <dassweta1997@gmail.com>
2023-03-18 23:56:16 +0545

    adding recommendation

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Flutter\\Final Apps\\servemandu_users_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "PROJECT_DIR=D:\\Flutter\\Final Apps\\servemandu_users_app"
+  "FLUTTER_ROOT=D:\\Flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Flutter\\Final Apps\\servemandu_users_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Flutter\\Final Apps\\servemandu_users_app"
+  "FLUTTER_TARGET=D:\\Flutter\\Final Apps\\servemandu_users_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Flutter\\Final Apps\\servemandu_users_app\\.dart_tool\\package_config.json"
+)
